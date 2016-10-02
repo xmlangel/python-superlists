6fdd56ae5dc0fec5fdeeb2e7eea22fc45d26f8e6
kwangmyung Kim <kmkim@whatap.io>
2016-10-03 01:04:45 +0900

    chrome으로 변경

--- project/functional_test.py
+++ project/functional_test.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser = get('http://localhost:8000')
+browser = webdriver.Chrome('./chromedriver')
+
+browser.get('http://localhost:8000')
 
 assert 'Django' in browser.title
